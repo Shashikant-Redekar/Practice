8c66bf5ac940074a2d8f04be81e30a93e6c238c8
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2026-02-16 23:44:07 +0530

    Update Best_Time_to_Buy_and_Sell_Stock.java

--- Best_Time_to_Buy_and_Sell_Stock.java
+++ Best_Time_to_Buy_and_Sell_Stock.java
@@ -45,3 +45,19 @@ class Solution {
       return (Math.max(prevResult,result));
     }
 }
+
+// new solution 
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];  // better day to buy
+            } else {
+                int profit = prices[i] - minPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+        }
+
+        return maxProfit;
