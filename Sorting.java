4dac4dd3a121b94065c914a30a819d3822bd3459
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2026-03-23 23:13:20 +0530

    Add bubble sort implementation to reverse array
    Implemented bubble sort algorithm in the Solution class to reverse an array.

--- Sorting.java
+++ Sorting.java
@@ -14,3 +14,28 @@ class Solution {
             System.out.println(i);
         }
 }
+
+
+// BUBBLE SORT
+
+class Solution {
+    void reverse(int[] arr) {
+        for(int i = arr.length -1; i > 0; i--){
+            int swap = 0;
+            for (int j = 0; j < i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    System.out.println("run");
+                    swap = 1;
+                }
+            }
+            if (swap == 0)
+                break;
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
