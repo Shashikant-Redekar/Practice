a728f22828ba97bd177a496cef08a6c5f45f8a3e
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2026-03-24 00:17:36 +0530

    Add recursive insertion sort implementation
    Implemented insertion sort using recursion in the Solution class.

--- Sorting.java
+++ Sorting.java
@@ -59,3 +59,19 @@ class Solution {
         }
     }
 }
+
+// INSERSION SORT USING RECURSION
+
+class Solution {
+    void sort(int[] arr, int n, int i) {
+        if (n == i) return;
+        int j = i;
+        while ( j > 0 && arr[j-1] > arr[j]){
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+        }
+        sort(arr,n,i+1);
+    }
+}
