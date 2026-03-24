9fa99a734068a9dcf9b52590d95ed2497463bdd3
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2026-03-24 22:54:27 +0530

    Implement secondLargest method in Solution class
    Add method to find the second largest element in an array.

--- Array_easy.java
+++ Array_easy.java
@@ -8,3 +8,22 @@
         }
         System.out.println(max);
   }
+
+//SecondLagest element in the array
+
+class Solution {
+    void secondLargest(int[] arr) {
+        int max = arr[0];
+        int smax = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(max < arr[i]) {
+                smax=max;
+                max = arr[i];
+            }
+            if(smax < arr[i] && arr[i] < max)
+                smax = arr[i];
+        }
+        System.out.println(max);
+        System.out.println(smax);
+    }
+}
