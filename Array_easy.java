8da4e2863ceb07c1909ef141497be72e9b008cfd
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2026-08-16 23:30:52 +0530

    Merge sorted arrays

--- Array_easy.java
+++ Array_easy.java
@@ -99,3 +99,44 @@ class Solution {
     }
 }
 }
+
+//Merge sorted arrays
+class Solution{
+    public void UnionoOfSortedArrays(int[] arr1,int[] arr2) {
+        List<Integer> union = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i < arr1.length &&  j < arr2.length){
+            if(arr1[i] < arr2[j]){
+                if(union.isEmpty() || arr1[i] != union.get(union.size() - 1)) {
+                    union.add(arr1[i]);
+                }
+                i++;
+            } else if(arr1[i] > arr2[j]){
+                if(union.isEmpty() || arr2[j] != union.get(union.size() - 1)) {
+                    union.add(arr2[j]);
+                }
+                j++;
+            } else {
+                if(union.isEmpty() || arr1[i] != union.get(union.size() - 1)) {
+                    union.add(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        while(i < arr1.length){
+            if(arr1[i] != union.get(union.size() - 1)){
+                union.add(arr1[i]);
+            }
+            i++;
+        }
+        while(j < arr2.length){
+            if(arr2[j] != union.get(union.size() -1)) {
+                union.add(arr2[j]);
+            }
+            j++;
+        }
+        System.out.println(union);
+    }
+}
