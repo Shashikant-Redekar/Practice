ee8310199c3f1aeb5bb3d6ba2c374f11736f739c
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2026-08-18 00:13:29 +0530

    Implement longestCommonPrefix method
    Added a method to find the longest common prefix from an array of strings.

--- String_easy.java
+++ String_easy.java
@@ -18,3 +18,21 @@ class Solution {
         return ret;
     }
 }
+
+//Longest Common Prefix
+class Solution {
+    public String longestCommonPrefix(String[] str) {
+        StringBuffer ans = new StringBuffer();
+        Arrays.sort(str);
+        String first = str[0];
+        String last = str[str.length -1];
+        for(int i = 0; i < Math.min(first.length(),last.length()); i++){
+            if(first.charAt(i) != last.charAt(i)){
+                return ans.toString();
+            } else {
+                ans.append(first.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+}
