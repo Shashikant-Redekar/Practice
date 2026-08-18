77e3e872d6474d052ce82797f4465c225c3d8cfc
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2026-08-18 23:24:04 +0530

    Add isIsomorphic method to Solution class
    Implement isIsomorphic method to check if two strings are isomorphic.

--- String_easy.java
+++ String_easy.java
@@ -36,3 +36,27 @@ class Solution {
         return ans.toString();
     }
 }
+
+//Isomorphic String
+class Solution {
+    public boolean isIsomorphic(String a, String b) {
+        HashMap<Character,Character> map = new HashMap<>();
+        if(a.length() != b.length()){
+            System.out.println("false");
+        }
+        for(int i =0; i< a.length();i++){
+            if(!map.containsKey(a.charAt(i))){
+                if(!map.containsValue(b.charAt(i))){
+                    map.put(a.charAt(i),b.charAt(i));
+                }else{
+                    return false;
+                }
+            }else{
+                if(map.get(a.charAt(i)) != b.charAt(i)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
