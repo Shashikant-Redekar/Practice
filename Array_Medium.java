852b51964af0b5fed08a7a294999b0027c3c1d2c
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2026-08-26 01:16:05 +0530

    Add sortColors method to sort colors in array
    Implemented Dutch National Flag algorithm to sort an array of 0's, 1's, and 2's in-place.

--- Array_Medium.java
+++ Array_Medium.java
@@ -12,3 +12,38 @@ class Solution {
         return new int[] {-1,-1};
     }
 }
+
+// sort 0's, 1's and 2's
+class Solution {
+    public void sortColors(int[] a) {
+        int high = a.length - 1;
+        int low = 0;
+        int mid = 0;
+        while(high > mid){
+            if(a[low] == 0){
+                low++;
+            }
+            if(a[mid] <= 1 ){
+                mid++;
+            }
+            if(a[high] == 2){
+                high--;
+            }
+            if(a[low] > a[high] && high > low && a[low] == 2){
+                int temp = a[low];
+                a[low] = a[high];
+                a[high] = temp;
+            }
+            if(a[mid] > a[high] && high > mid && a[mid] == 2){
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+            }
+            if(a[low] > a[mid] && mid > low && a[low] == 1){
+                int temp = a[low];
+                a[low] = a[mid];
+                a[mid] = temp;
+            }
+        }
+    }
+}
